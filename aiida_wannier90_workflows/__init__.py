8bbb4b48738c855559febd76eed69c57bef38a01
Junfeng Qiao <qiaojunfeng@outlook.com>
2020-03-19 09:28:03 +0100

    version 1.0.1

--- aiida_wannier90_workflows/__init__.py
+++ aiida_wannier90_workflows/__init__.py
@@ -8,4 +8,4 @@ AiiDA Wannier90 Workchain
 
 __authors__ = "Antimo Marrazzo, Giovanni Pizzi, Junfeng Qiao"
 ## If upgraded, remember to change it also in setup.json (for pip)
-__version__ = "1.0.0"
+__version__ = "1.0.1"
