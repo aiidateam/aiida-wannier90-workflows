e5b1d04c92815d09214b1069812d9a7cd289f122
Junfeng Qiao <qiaojunfeng@outlook.com>
2020-10-15 18:42:34 +0200

    Modularize into functions for code reuse
    So the functions can be imported and reused by users.

--- aiida_wannier90_workflows/tools/compare_dft_wannier_bands.py
+++ aiida_wannier90_workflows/tools/compare_dft_wannier_bands.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env runaiida
 """compare DFT and Wannier band structures
 """
-import argparse
 from aiida import orm
 
 def required_length(nmin, nmax):
@@ -14,22 +13,7 @@ def required_length(nmin, nmax):
             setattr(args, self.dest, values)
     return RequiredLength
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-    f'Plot DFT and Wannier bands for comparison.')
-    parser.add_argument('pk', metavar='PK', type=int, nargs='+', action=required_length(1,2), help='The PK of a Wannier90BandsWorkChain, the `compare_dft_bands` inputs of the Wannier90BandsWorkChain should be True; or PKs of 2 BandsData to be compared.')
-    parser.add_argument('-s', '--save', action='store_true', help="save as a python plotting script instead of showing matplotlib window")
-    args = parser.parse_args()
-
-    input_is_workchain = len(args.pk) == 1
-    if input_is_workchain:
-        workchain = orm.load_node(args.pk[0])
-        dft_bands = workchain.outputs.dft_bands
-        wan_bands = workchain.outputs.wannier90_interpolated_bands
-    else:
-        dft_bands = orm.load_node(args.pk[0])
-        wan_bands = orm.load_node(args.pk[1])
-
+def get_mpl_code_for_bands(dft_bands, wan_bands, title=None, save=False, filename=None):
     # dft_bands.show_mpl()
     dft_mpl_code = dft_bands._exportcontent(fileformat='mpl_singlefile', legend=f'{dft_bands.pk}', main_file_name='')[0]
     wan_mpl_code = wan_bands._exportcontent(fileformat='mpl_singlefile', legend=f'{wan_bands.pk}', main_file_name='',
@@ -40,22 +24,49 @@ if __name__ == '__main__':
     wan_mpl_code = wan_mpl_code.replace(b'p = fig.add_subplot(1,1,1)', b'')
     mpl_code = dft_mpl_code + wan_mpl_code
 
-    formula = workchain.inputs.structure.get_formula()
-    # add title
-    if input_is_workchain:
-        replacement = f'workchain pk {workchain.pk}, {formula}, dft_bands pk {dft_bands.pk}, wan_bands pk {wan_bands.pk}'
-    else:
-        replacement = f'1st bands pk {dft_bands.pk}, 2nd bands pk {wan_bands.pk}'
-    replacement = f'p.set_title("{replacement}")\npl.show()'
+    if title is None:
+        title = f'1st bands pk {dft_bands.pk}, 2nd bands pk {wan_bands.pk}'
+    replacement = f'p.set_title("{title}")\npl.show()'
     mpl_code = mpl_code.replace(b'pl.show()', replacement.encode())
 
-    if input_is_workchain:
-        fname = f'bandsdiff_{formula}_{workchain.pk}.py'
-    else:
-        fname = f'bandsdiff_{dft_bands.pk}_{wan_bands.pk}.png'
-
-    if args.save:
-        with open(fname, 'w') as f:
+    if save:
+        if filename is None:
+            filename = f'bandsdiff_{dft_bands.pk}_{wan_bands.pk}.py'
+        with open(filename, 'w') as f:
             f.write(mpl_code.decode())
+
+    return mpl_code
+
+def get_mpl_code_for_workchain(workchain, title=None, save=False, filename=None):
+    dft_bands = workchain.outputs.dft_bands
+    wan_bands = workchain.outputs.wannier90_interpolated_bands
+
+    formula = workchain.inputs.structure.get_formula()
+    if title is None:
+        title = f'workchain pk {workchain.pk}, {formula}, dft_bands pk {dft_bands.pk}, wan_bands pk {wan_bands.pk}'
+
+    if save and (filename is None):
+        filename = f'bandsdiff_{formula}_{workchain.pk}.py'
+    mpl_code = get_mpl_code_for_bands(dft_bands, wan_bands, title, save, filename)
+
+    return mpl_code
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description=
+    f'Plot DFT and Wannier bands for comparison.')
+    parser.add_argument('pk', metavar='PK', type=int, nargs='+', action=required_length(1,2), help='The PK of a Wannier90BandsWorkChain, the `compare_dft_bands` inputs of the Wannier90BandsWorkChain should be True; or PKs of 2 BandsData to be compared.')
+    parser.add_argument('-s', '--save', action='store_true', help="save as a python plotting script instead of showing matplotlib window")
+    args = parser.parse_args()
+
+    input_is_workchain = len(args.pk) == 1
+    if input_is_workchain:
+        workchain = orm.load_node(args.pk[0])
+        mpl_code = get_mpl_code_for_workchain(workchain, save=args.save)
     else:
+        dft_bands = orm.load_node(args.pk[0])
+        wan_bands = orm.load_node(args.pk[1])
+        mpl_code = get_mpl_code_for_bands(dft_bands, wan_bands, save=args.save)
+
+    if not args.save:
         exec(mpl_code)
