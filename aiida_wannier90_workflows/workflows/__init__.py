984b9cc41d2bde0f09c5d0829bd8b834995cbe31
Junfeng Qiao <qiaojunfeng@outlook.com>
2021-07-28 21:13:09 +0200

    Update error handlers for bvectors

--- aiida_wannier90_workflows/workflows/__init__.py
+++ aiida_wannier90_workflows/workflows/__init__.py
@@ -6,10 +6,10 @@ AiiDA Wannier90 Workchain
 
 """
 
-from .bands import Wannier90BandsWorkChain
+from .base import Wannier90BaseWorkChain
 from .wannier import Wannier90WorkChain
-from .band_structure import PwBandStructureWorkChain
+from .bands import Wannier90BandsWorkChain
 
 __all__ = (
-    "Wannier90BandsWorkChain", "Wannier90WorkChain", "PwBandStructureWorkChain"
+    "Wannier90BaseWorkChain", "Wannier90WorkChain", "Wannier90BandsWorkChain"
 )
