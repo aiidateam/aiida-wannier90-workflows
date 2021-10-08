63fb81c1179559c468d0167fa4e80dc1a356ad18
Junfeng Qiao <qiaojunfeng@outlook.com>
2021-10-08 18:11:49 +0200

    Add error handler for 'not enough states' in wannier90 disentanglement
    Now the `Wannier90BandsWorkChain` calls `Wannier90BaseWorkChain` in the
    final wannierise step, while previously it directly calls
    `Wannier90Calculation`. This allows handling errors in disentanglement.

--- aiida_wannier90_workflows/cli/node.py
+++ aiida_wannier90_workflows/cli/node.py
@@ -2,22 +2,22 @@
 """Command line interface `aiida-wannier90-workflows`."""
 import click
 from aiida import orm
-from aiida.cmdline.params import arguments, options, types
-from aiida.cmdline.utils import decorators, echo
+from aiida.cmdline.params import arguments
+from aiida.cmdline.utils import echo
 
 from .root import cmd_root
 
 
 @cmd_root.group('node')
 def cmd_node():  # pylint: disable=unused-argument
-    """Inspect a node"""
+    """Inspect a node."""
 
 
 @cmd_node.command('show')
 @arguments.NODES()
 @click.pass_context
 def cmd_node_show(ctx, nodes):  # pylint: disable=unused-argument
-    """Show info of a node"""
+    """Show info of a node."""
     from aiida.cmdline.commands.cmd_node import node_show
 
     for node in nodes:
@@ -29,6 +29,7 @@ def cmd_node_show(ctx, nodes):  # pylint: disable=unused-argument
 
 
 def get_last_calcjob(workchain: orm.WorkChainNode) -> orm.CalcJobNode:
+    """Return the last CalcJob of a WorkChain."""
     calcs = []
     for called_descendant in workchain.called_descendants:
         if not isinstance(called_descendant, orm.CalcJobNode):
@@ -55,18 +56,18 @@ def get_last_calcjob(workchain: orm.WorkChainNode) -> orm.CalcJobNode:
     required=False,
     help='Goto the calcjob with this call link label.'
 )
-@click.pass_context
+@click.pass_context  # pylint: disable=too-many-statements
 def cmd_node_gotocomputer(ctx, node, link_label):
-    """Open a shell in the remote folder of the calcjob, or the last calcjob of the workflow"""
+    """Open a shell in the remote folder of the calcjob, or the last calcjob of the workflow."""
     import os
     from aiida.common.exceptions import NotExistent
     from aiida.common.links import LinkType
     from aiida.cmdline.commands.cmd_calcjob import calcjob_gotocomputer
     from aiida.plugins import DataFactory
 
-    RemoteData = DataFactory("remote")
-    RemoteStashFolderData = DataFactory("remote.stash.folder")
-    FolderData = DataFactory("folder")
+    RemoteData = DataFactory('remote')  # pylint: disable=invalid-name
+    RemoteStashFolderData = DataFactory('remote.stash.folder')  # pylint: disable=invalid-name
+    FolderData = DataFactory('folder')  # pylint: disable=invalid-name
 
     echo.echo(f'Node<{node.pk}> type {type(node)}')
 
@@ -76,26 +77,23 @@ def cmd_node_gotocomputer(ctx, node, link_label):
     elif isinstance(node, orm.WorkChainNode):
         if link_label is None:
             last_calcjob = get_last_calcjob(node)
+            if last_calcjob is None:
+                echo.echo(f'No CalcJob for {node}?')
+                return
+
             # Get call link label
-            link_triples = node.get_outgoing().link_triples
-            link = list(
-                filter(lambda x: x.node == last_calcjob, link_triples)
-            )[0]
+            link_triples = node.get_outgoing(link_type=(LinkType.CALL_CALC, LinkType.CALL_WORK)).link_triples
+            link = list(filter(lambda _: last_calcjob in _.node.called_descendants, link_triples))[0]
             link_label = link.link_label
         else:
             try:
-                called = node.get_outgoing(link_label_filter=link_label
-                                           ).one().node
-            except ValueError as exc:
-                link_triples = node.get_outgoing(
-                    link_type=(LinkType.CALL_CALC, LinkType.CALL_WORK)
-                ).link_triples
+                called = node.get_outgoing(link_label_filter=link_label).one().node
+            except ValueError:
+                link_triples = node.get_outgoing(link_type=(LinkType.CALL_CALC, LinkType.CALL_WORK)).link_triples
                 valid_lables = [x.link_label for x in link_triples]
                 valid_lables = '\n'.join(valid_lables)
-                echo.echo(
-                    f"No nodes found with call link label '{link_label}', valid labels are:"
-                )
-                echo.echo(f"{valid_lables}")
+                echo.echo(f"No nodes found with call link label '{link_label}', valid labels are:")
+                echo.echo(f'{valid_lables}')
                 return
 
             if isinstance(called, orm.CalcJobNode):
@@ -106,9 +104,9 @@ def cmd_node_gotocomputer(ctx, node, link_label):
                 echo.echo(f'Unsupported type of node: {called}')
                 return
 
-        msg = f"Parent WorkChain: {node.process_label}<{node.pk}>\n"
-        msg += f" Lastest CalcJob: {last_calcjob.process_label}<{last_calcjob.pk}>\n"
-        msg += f" Call link label: {link_label}\n"
+        msg = f'Parent WorkChain: {node.process_label}<{node.pk}>\n'
+        msg += f' Lastest CalcJob: {last_calcjob.process_label}<{last_calcjob.pk}>\n'
+        msg += f' Call link label: {link_label}\n'
         echo.echo(msg)
 
         ctx.invoke(calcjob_gotocomputer, calcjob=last_calcjob)
@@ -134,7 +132,7 @@ def cmd_node_gotocomputer(ctx, node, link_label):
     elif isinstance(node, FolderData):
         # Seems FolderData.computer is None
         # I assume the repository is on localhost
-        workdir = node._repository._get_base_folder().abspath
+        workdir = node._repository._get_base_folder().abspath  # pylint: disable=protected-access
         command = f'cd {workdir}; bash -i'
         echo.echo_info('going to the work directory...')
         os.system(command)
@@ -146,7 +144,7 @@ def cmd_node_gotocomputer(ctx, node, link_label):
 @cmd_node.command('cleanworkdir')
 @arguments.NODES()
 def cmd_node_clean(nodes):
-    """Clean the workdir of CalcJobNode/WorkChainNode"""
+    """Clean the workdir of CalcJobNode/WorkChainNode."""
 
     for node in nodes:
         calcs = []
@@ -168,6 +166,4 @@ def cmd_node_clean(nodes):
             except (IOError, OSError, KeyError):
                 pass
         if cleaned_calcs:
-            echo.echo(
-                f"cleaned remote folders of calculations: {' '.join(map(str, cleaned_calcs))}"
-            )
+            echo.echo(f"cleaned remote folders of calculations: {' '.join(map(str, cleaned_calcs))}")
--- aiida_wannier90_workflows/workflows/base.py
+++ aiida_wannier90_workflows/workflows/base.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Wrapper workchain for Wannier90Calculation to automatically handle several errors."""
 from aiida import orm
 from aiida.common import AttributeDict
 from aiida.engine import while_
@@ -13,9 +14,12 @@ class Wannier90BaseWorkChain(BaseRestartWorkChain):
     _process_class = Wannier90Calculation
 
     _WANNIER90_DEFAULT_KMESH_TOL = 1e-6
+    _WANNIER90_DEFAULT_DIS_PROJ_MIN = 0.1
+    _WANNIER90_DEFAULT_DIS_PROJ_MAX = 0.9
 
     @classmethod
     def define(cls, spec):
+        """Define the process spec."""
         super().define(spec)
         spec.expose_inputs(Wannier90Calculation, namespace='wannier90')
 
@@ -30,21 +34,19 @@ class Wannier90BaseWorkChain(BaseRestartWorkChain):
 
         spec.expose_outputs(Wannier90Calculation)
 
-        spec.exit_code(
-            400, 'ERROR_BVECTORS', message='Unrecoverable bvectors error.'
-        )
+        spec.exit_code(400, 'ERROR_BVECTORS', message='Unrecoverable bvectors error.')
+        spec.exit_code(401, 'ERROR_DISENTANGLEMENT_NOT_ENOUGH_STATES', message='Unrecoverable disentanglement error.')
 
     def setup(self):
         """Call the `setup` of the `BaseRestartWorkChain` and then create the inputs dictionary in `self.ctx.inputs`.
 
-        This `self.ctx.inputs` dictionary will be used by the `BaseRestartWorkChain` to submit the calculations in the
-        internal loop.
+        This `self.ctx.inputs` dictionary will be used by the `BaseRestartWorkChain` to submit
+        the calculations in the internal loop.
         """
         super().setup()
-        self.ctx.inputs = AttributeDict(
-            self.exposed_inputs(Wannier90Calculation, 'wannier90')
-        )
+        self.ctx.inputs = AttributeDict(self.exposed_inputs(Wannier90Calculation, 'wannier90'))
         self.ctx.kmeshtol_new = [self._WANNIER90_DEFAULT_KMESH_TOL, 1e-8, 1e-4]
+        self.ctx.disprojmin_multipliers = [0.5, 0.25, 0.125]
 
     def report_error_handled(self, calculation, action):
         """Report an action taken for a calculation that has failed.
@@ -54,35 +56,66 @@ class Wannier90BaseWorkChain(BaseRestartWorkChain):
         :param calculation: the failed calculation node
         :param action: a string message with the action taken
         """
-        arguments = [calculation.process_label, calculation.pk, calculation.exit_status, calculation.exit_message]
-        self.report('{}<{}> failed with exit status {}: {}'.format(*arguments))
+        message = f'{calculation.process_label}<{calculation.pk}> failed'
+        message += f' with exit status {calculation.exit_status}: {calculation.exit_message}'
+        self.report(message)
         self.report(f'Action taken: {action}')
 
-    @process_handler(exit_codes=Wannier90Calculation.exit_codes.ERROR_BVECTORS)
+    @process_handler(exit_codes=[Wannier90Calculation.exit_codes.ERROR_BVECTORS])
     def handle_bvectors(self, calculation):
         """Try to fix Wannier90 bvectors errors by tunning `kmesh_tol`.
+
         The handler will try to use kmesh_tol = 1e-6, 1e-8, 1e-4.
         """
-        KMESHTOL_KEY = 'kmesh_tol'
         parameters = self.ctx.inputs.parameters.get_dict()
 
         # If the user has specified `kmesh_tol` in the input parameters and it is different
         # from the default, we will first try to use the default `kmesh_tol`.
-        if KMESHTOL_KEY in parameters:
-            kmeshtol_cur = parameters[KMESHTOL_KEY]
-        else:
-            kmeshtol_cur = self._WANNIER90_DEFAULT_KMESH_TOL
-        if kmeshtol_cur in self.ctx.kmeshtol_new:
-            self.ctx.kmeshtol_new.remove(kmeshtol_cur)
+        current_kmeshtol = parameters.get('kmesh_tol', self._WANNIER90_DEFAULT_KMESH_TOL)
+        if current_kmeshtol in self.ctx.kmeshtol_new:
+            self.ctx.kmeshtol_new.remove(current_kmeshtol)
 
         if len(self.ctx.kmeshtol_new) == 0:
             action = 'Unrecoverable bvectors error after several trials of kmesh_tol'
             self.report_error_handled(calculation, action)
             return ProcessHandlerReport(True, self.exit_codes.ERROR_BVECTORS)
 
-        kmeshtol_new = self.ctx.kmeshtol_new.pop(0)
-        parameters[KMESHTOL_KEY] = kmeshtol_new
-        action = f'Bvectors error, current kmesh_tol = {kmeshtol_cur}, new kmesh_tol = {kmeshtol_new}'
+        new_kmeshtol = self.ctx.kmeshtol_new.pop(0)
+        parameters['kmesh_tol'] = new_kmeshtol
+        action = f'Bvectors error, current kmesh_tol = {current_kmeshtol}, new kmesh_tol = {new_kmeshtol}'
+        self.report_error_handled(calculation, action)
+        self.ctx.inputs.parameters = orm.Dict(dict=parameters)
+
+        return ProcessHandlerReport(True)
+
+    @process_handler(exit_codes=[Wannier90Calculation.exit_codes.ERROR_DISENTANGLEMENT_NOT_ENOUGH_STATES])
+    def handle_disentanglement_not_enough_states(self, calculation):
+        """Try to fix Wannier90 wout error message related to projectability disentanglement.
+
+        The error message is: 'Energy window contains fewer states than number of target WFs,
+        consider reducing dis_proj_min/increasing dis_win_max?'.
+
+        The handler will try to use decrease 'dis_proj_min' to allow for more states for disentanglement.
+        """
+        parameters = self.ctx.inputs.parameters.get_dict()
+        if 'dis_proj_min' not in parameters and 'dis_proj_max' not in parameters:
+            # If neither is present, I should never encounter this exit_code
+            action = 'Unrecoverable bvectors error: the error handler is only for projectability disentanglement'
+            self.report_error_handled(calculation, action)
+            return ProcessHandlerReport(True, self.exit_codes.ERROR_DISENTANGLEMENT_NOT_ENOUGH_STATES)
+
+        if len(self.ctx.disprojmin_multipliers) == 0:
+            action = 'Unrecoverable error after several trials of dis_proj_min'
+            self.report_error_handled(calculation, action)
+            return ProcessHandlerReport(True, self.exit_codes.ERROR_DISENTANGLEMENT_NOT_ENOUGH_STATES)
+
+        current_disprojmin = parameters.get('dis_proj_min', self._WANNIER90_DEFAULT_KMESH_TOL)
+        multiplier = self.ctx.disprojmin_multipliers.pop(0)
+        new_disprojmin = current_disprojmin * multiplier
+        parameters['dis_proj_min'] = new_disprojmin
+
+        action = 'Not enough states for disentanglement, '
+        action += f'current dis_proj_min = {current_disprojmin}, new dis_proj_min = {new_disprojmin}'
         self.report_error_handled(calculation, action)
         self.ctx.inputs.parameters = orm.Dict(dict=parameters)
 
