7d6e3c92675c429ed85cb7038d27bb125e19dfbe
Junfeng Qiao <qiaojunfeng@outlook.com>
2021-10-28 16:31:13 +0200

    Print structure in CLI list

--- aiida_wannier90_workflows/cli/list.py
+++ aiida_wannier90_workflows/cli/list.py
@@ -1,41 +1,107 @@
 # -*- coding: utf-8 -*-
 """Commands to list instances of `PseudoPotentialFamily`."""
-from pprint import pprint
 import click
 from aiida import orm
 from aiida.cmdline.params import options as options_core
 from aiida.cmdline.utils import decorators, echo
 from aiida.cmdline.utils.query.calculation import CalculationQueryBuilder
-from aiida.cmdline.commands.cmd_process import process_list
 
 from .root import cmd_root
 
 
 @cmd_root.command('list')
-@options_core.GROUP(
-    help='Only include entries that are a member of this group.'
-)
 @options_core.PROJECT(
-    type=click.Choice(CalculationQueryBuilder.valid_projections),
-    default=CalculationQueryBuilder.default_projections
+    type=click.Choice(CalculationQueryBuilder.valid_projections), default=CalculationQueryBuilder.default_projections
 )
+@options_core.ORDER_BY()
+@options_core.ORDER_DIRECTION()
+@options_core.GROUP(help='Only include entries that are a member of this group.')
+@options_core.ALL(help='Show all entries, regardless of their process state.', default=True)
+@options_core.PROCESS_STATE()
+@options_core.PAUSED()
+@options_core.EXIT_STATUS()
+@options_core.FAILED()
+@options_core.PAST_DAYS()
+@options_core.LIMIT()
 @options_core.RAW()
 @decorators.with_dbenv()
-@click.pass_context
-def cmd_list(ctx, group, project, raw):
+@click.pass_context  # pylint: disable=too-many-statements
+def cmd_list(
+    ctx,  # pylint: disable=unused-argument
+    all_entries,
+    group,
+    process_state,
+    paused,
+    exit_status,
+    failed,
+    past_days,
+    limit,
+    project,
+    raw,
+    order_by,
+    order_dir
+):
     """List all instances of `Wannier90BandsWorkChain`."""
     from tabulate import tabulate
+    from aiida.cmdline.commands.cmd_process import process_list  # pylint: disable=unused-import
 
     process_label = 'Wannier90BandsWorkChain'
 
-    result = ctx.invoke(
-        process_list,
-        all_entries=True,
-        group=group,
-        process_label=process_label,
-        project=project,
-        raw=raw,
+    # result = ctx.invoke(
+    #     process_list,
+    #     all_entries=True,
+    #     group=group,
+    #     process_label=process_label,
+    #     project=project,
+    #     raw=raw,
+    # )
+
+    # Copied from process_list
+    from aiida.cmdline.utils.common import print_last_process_state_change, check_worker_load
+
+    relationships = {}
+
+    if group:
+        relationships['with_node'] = group
+
+    builder = CalculationQueryBuilder()
+    filters = builder.get_filters(all_entries, process_state, process_label, paused, exit_status, failed)
+    query_set = builder.get_query_set(
+        relationships=relationships, filters=filters, order_by={order_by: order_dir}, past_days=past_days, limit=limit
     )
+    projected = builder.get_projected(query_set, projections=project)
+
+    headers = projected.pop(0)
+
+    projected_with_structure = []
+    # Add structure
+    for entry in projected:
+        # I assume 0th column is PK
+        pk = entry[0]
+        node = orm.load_node(pk)
+        formula = node.inputs.structure.get_formula()
+        entry_with_structure = [pk, formula, *entry[1:]]
+        projected_with_structure.append(entry_with_structure)
+    projected = projected_with_structure
+    headers = [headers[0], 'structure', *headers[1:]]
+
+    if raw:
+        tabulated = tabulate(projected, tablefmt='plain')
+        echo.echo(tabulated)
+    else:
+        tabulated = tabulate(projected, headers=headers)
+        echo.echo(tabulated)
+        echo.echo(f'\nTotal results: {len(projected)}\n')
+        print_last_process_state_change()
+        # Second query to get active process count
+        # Currently this is slow but will be fixed wiith issue #2770
+        # We place it at the end so that the user can Ctrl+C after getting the process table.
+        builder = CalculationQueryBuilder()
+        filters = builder.get_filters(process_state=('created', 'waiting', 'running'))
+        query_set = builder.get_query_set(filters=filters)
+        projected = builder.get_projected(query_set, projections=['pk'])
+        worker_slot_use = len(projected) - 1
+        check_worker_load(worker_slot_use)
 
     # Collect statistics of failed workflows
     # similar to aiida.cmdline.commands.cmd_process.process_list
@@ -46,57 +112,40 @@ def cmd_list(ctx, group, project, raw):
 
     builder = CalculationQueryBuilder()
     filters = builder.get_filters(
-        all_entries=True,
-        process_state=None,
-        process_label=process_label,
-        paused=False,
-        exit_status=None,
-        failed=False
+        all_entries=True, process_state=None, process_label=process_label, paused=False, exit_status=None, failed=False
     )
     query_set = builder.get_query_set(
-        relationships=relationships,
-        filters=filters,
-        order_by={'ctime': 'asc'},
-        past_days=None,
-        limit=None
+        relationships=relationships, filters=filters, order_by={'ctime': 'asc'}, past_days=None, limit=None
     )
     state_projections = (
-        'pk', 'state', 'process_state', 'process_status', 'exit_status',
-        'job_state', 'scheduler_state', 'exception'
+        'pk', 'state', 'process_state', 'process_status', 'exit_status', 'job_state', 'scheduler_state', 'exception'
     )
     projected = builder.get_projected(query_set, projections=state_projections)
     headers = projected.pop(0)
     # pprint(projected)
 
-    state_projections_idx = {
-        state_projections[i]: i
-        for i in range(len(state_projections))
-    }
+    state_projections_idx = {state_projections[i]: i for i in range(len(state_projections))}
     excepted_workflows = {}
     num_success = 0
     num_excepted = 0
-    for wc in projected:
-        pk = str(wc[state_projections_idx['pk']])
-        process_state = wc[state_projections_idx['process_state']]
-        exit_status = str(wc[state_projections_idx['exit_status']])
+    for entry in projected:
+        pk = str(entry[state_projections_idx['pk']])
+        process_state = entry[state_projections_idx['process_state']]
+        exit_status = str(entry[state_projections_idx['exit_status']])
 
         if process_state == 'Finished':
             if exit_status == '0':
                 num_success += 1
                 continue
-            elif exit_status is None:
-                raise Exception(
-                    f'{process_label}<{pk}> process_state = Finished but exit_status is None?'
-                )
+            if exit_status is None:
+                raise Exception(f'{process_label}<{pk}> process_state = Finished but exit_status is None?')
 
             if exit_status not in excepted_workflows:
                 excepted_workflows[exit_status] = []
             excepted_workflows[exit_status].append(pk)
         else:
             if process_state is None:
-                raise Exception(
-                    f'{process_label}<{pk}> process_state is None?'
-                )
+                raise Exception(f'{process_label}<{pk}> process_state is None?')
 
             if process_state not in excepted_workflows:
                 excepted_workflows[process_state] = []
@@ -105,8 +154,7 @@ def cmd_list(ctx, group, project, raw):
         num_excepted += 1
 
     # print(excepted_workflows)
-    data = [[k, str(len(v)), ' '.join(v)]
-            for k, v in excepted_workflows.items()]
+    data = [[k, str(len(v)), ' '.join(v)] for k, v in excepted_workflows.items()]
     data.append(['-' * 8, None, None])
     data.append(['Total excepted', num_excepted, None])
     data.append(['Total success', num_success, None])
@@ -116,5 +164,3 @@ def cmd_list(ctx, group, project, raw):
 
     tabulated = f'\n{tabulated}'
     echo.echo(tabulated)
-
-    return result
