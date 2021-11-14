9680d074df9db3da0de776a29b3de4fb7a5fbc2e
Junfeng Qiao <qiaojunfeng@outlook.com>
2021-11-14 20:49:33 +0100

    Explicitly set exit code in decorator

--- aiida_wannier90_workflows/workflows/base/opengrid.py
+++ aiida_wannier90_workflows/workflows/base/opengrid.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Wrapper workchain for OpengridCalculation to automatically handle several errors."""
 from aiida_quantumespresso.calculations.opengrid import OpengridCalculation
+from aiida.engine import process_handler
 from .qebaserestart import QeBaseRestartWorkChain
 
 
@@ -9,3 +10,8 @@ class OpengridBaseWorkChain(QeBaseRestartWorkChain):
 
     _process_class = OpengridCalculation
     _expose_inputs_namespace = 'opengrid'
+
+    @process_handler(exit_codes=[_process_class.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE])  # pylint: disable=no-member
+    def handle_output_stdout_incomplete(self, calculation):
+        """Overide parent function."""
+        return super().handle_output_stdout_incomplete(calculation)
