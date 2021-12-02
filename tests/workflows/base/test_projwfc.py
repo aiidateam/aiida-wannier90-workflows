65875986a84619574d67f94056e0cb8c4fe7f9fb
Junfeng Qiao <qiaojunfeng@outlook.com>
2021-12-02 13:17:47 +0100

    Add tests for base workchains

--- tests/workflows/base/test_projwfc.py
+++ tests/workflows/base/test_projwfc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Tests for the `OpengridBaseWorkChain` class."""
+"""Tests for the `ProjwfcBaseWorkChain` class."""
 import pytest
 
 from plumpy import ProcessState
@@ -7,25 +7,25 @@ from plumpy import ProcessState
 from aiida.common import AttributeDict
 from aiida.engine import ProcessHandlerReport
 
-from aiida_quantumespresso.calculations.opengrid import OpengridCalculation
+from aiida_quantumespresso.calculations.projwfc import ProjwfcCalculation
 
-from aiida_wannier90_workflows.workflows.base.opengrid import OpengridBaseWorkChain
+from aiida_wannier90_workflows.workflows.base.projwfc import ProjwfcBaseWorkChain
 
 # pylint: disable=no-member,redefined-outer-name
 
 
 @pytest.fixture
-def generate_inputs_opengrid(fixture_sandbox, fixture_localhost, fixture_code, generate_remote_data):
-    """Generate default inputs for a `OpengridCalculation."""
+def generate_inputs_projwfc(fixture_sandbox, fixture_localhost, fixture_code, generate_remote_data):
+    """Generate default inputs for a `ProjwfcCalculation."""
 
-    def _generate_inputs_opengrid():
-        """Generate default inputs for a `OpengridCalculation."""
+    def _generate_inputs_projwfc():
+        """Generate default inputs for a `ProjwfcCalculation."""
         from aiida_quantumespresso.utils.resources import get_default_options
 
         inputs = {
-            'code': fixture_code('quantumespresso.opengrid'),
+            'code': fixture_code('quantumespresso.projwfc'),
             'parent_folder':
-            generate_remote_data(fixture_localhost, fixture_sandbox.abspath, 'quantumespresso.opengrid'),
+            generate_remote_data(fixture_localhost, fixture_sandbox.abspath, 'quantumespresso.projwfc'),
             'metadata': {
                 'options': get_default_options()
             }
@@ -33,24 +33,22 @@ def generate_inputs_opengrid(fixture_sandbox, fixture_localhost, fixture_code, g
 
         return inputs
 
-    return _generate_inputs_opengrid
+    return _generate_inputs_projwfc
 
 
 @pytest.fixture
-def generate_workchain_opengrid(
-    generate_workchain, generate_inputs_opengrid, fixture_localhost, generate_calc_job_node
-):
-    """Generate an instance of a `OpengridBaseWorkChain`."""
+def generate_workchain_projwfc(generate_workchain, generate_inputs_projwfc, fixture_localhost, generate_calc_job_node):
+    """Generate an instance of a `ProjwfcBaseWorkChain`."""
 
-    def _generate_workchain_opengrid(exit_code=None, inputs=None, test_name=None):
-        entry_point = 'wannier90_workflows.base.opengrid'
+    def _generate_workchain_projwfc(exit_code=None, inputs=None, test_name=None):
+        entry_point = 'wannier90_workflows.base.projwfc'
         if not inputs:
-            inputs = {'opengrid': generate_inputs_opengrid()}
+            inputs = {'projwfc': generate_inputs_projwfc()}
         process = generate_workchain(entry_point, inputs)
 
         if exit_code is not None:
-            entry_point_calc_job = 'quantumespresso.opengrid'
-            node = generate_calc_job_node(entry_point_calc_job, fixture_localhost, test_name, inputs['opengrid'])
+            entry_point_calc_job = 'quantumespresso.projwfc'
+            node = generate_calc_job_node(entry_point_calc_job, fixture_localhost, test_name, inputs['projwfc'])
             node.set_process_state(ProcessState.FINISHED)
             node.set_exit_status(exit_code.status)
 
@@ -59,12 +57,12 @@ def generate_workchain_opengrid(
 
         return process
 
-    return _generate_workchain_opengrid
+    return _generate_workchain_projwfc
 
 
-def test_setup(generate_workchain_opengrid):
-    """Test `OpengridBaseWorkChain.setup`."""
-    process = generate_workchain_opengrid()
+def test_setup(generate_workchain_projwfc):
+    """Test `ProjwfcBaseWorkChain.setup`."""
+    process = generate_workchain_projwfc()
     process.setup()
 
     assert isinstance(process.ctx.inputs, AttributeDict)
@@ -78,13 +76,13 @@ def test_setup(generate_workchain_opengrid):
     '-nk',
     '-npools',
 ))
-def test_handle_output_stdout_incomplete(generate_workchain_opengrid, generate_inputs_opengrid, npool_key, npool_value):
-    """Test `OpengridBaseWorkChain.handle_output_stdout_incomplete` for restarting from OOM."""
+def test_handle_output_stdout_incomplete(generate_workchain_projwfc, generate_inputs_projwfc, npool_key, npool_value):
+    """Test `ProjwfcBaseWorkChain.handle_output_stdout_incomplete` for restarting from OOM."""
     from aiida import orm
 
-    inputs = {'opengrid': generate_inputs_opengrid()}
+    inputs = {'projwfc': generate_inputs_projwfc()}
     # E.g. when number of MPI procs = 4, the next trial is 2
-    inputs['opengrid']['metadata']['options'] = {
+    inputs['projwfc']['metadata']['options'] = {
         'resources': {
             'num_machines': 1,
             'num_mpiprocs_per_machine': npool_value
@@ -93,9 +91,9 @@ def test_handle_output_stdout_incomplete(generate_workchain_opengrid, generate_i
         'withmpi': True,
         'scheduler_stderr': '_scheduler-stderr.txt'
     }
-    inputs['opengrid']['settings'] = orm.Dict(dict={'cmdline': [npool_key, f'{npool_value}']})
-    process = generate_workchain_opengrid(
-        exit_code=OpengridCalculation.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE,
+    inputs['projwfc']['settings'] = orm.Dict(dict={'cmdline': [npool_key, f'{npool_value}']})
+    process = generate_workchain_projwfc(
+        exit_code=ProjwfcCalculation.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE,
         inputs=inputs,
         test_name='out_of_memory'
     )
@@ -116,7 +114,7 @@ def test_handle_output_stdout_incomplete(generate_workchain_opengrid, generate_i
     result = process.inspect_process()
     new_npool_value = npool_value // 4
     if new_npool_value == 0:
-        assert result == OpengridBaseWorkChain.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE
+        assert result == ProjwfcBaseWorkChain.exit_codes.ERROR_OUTPUT_STDOUT_INCOMPLETE
         new_npool_value = 1
     else:
         assert result.status == 0
