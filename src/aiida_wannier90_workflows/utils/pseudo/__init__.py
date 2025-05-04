a3a2b595024d62397479d5094e88bc84d18ef3ea
Yuhao Jiang <jiangyh@buaa.edu.cn>
2025-05-04 16:28:08 +0800

    Update logic in utils.pseudo.__init__
    update from loop over composition to loop over sites.

--- src/aiida_wannier90_workflows/utils/pseudo/__init__.py
+++ src/aiida_wannier90_workflows/utils/pseudo/__init__.py
@@ -269,11 +269,9 @@ def get_wannier_number_of_bands(  # pylint: disable=too-many-positional-argument
     """
     from .upf import get_upf_content, is_soc_pseudo
 
-    reduced_pseudos = reduce_pseudos(pseudos, structure)
     if spin_orbit_coupling:
-        composition = structure.get_composition()
-        for kind in composition:
-            upf = reduced_pseudos[kind]
+        for site in structure.sites:
+            upf = pseudos[site.kind_name]
             upf_content = get_upf_content(upf)
             if not is_soc_pseudo(upf_content):
                 raise ValueError("Should use SOC pseudo for SOC calculation")
@@ -327,11 +325,9 @@ def get_wannier_number_of_bands_ext(  # pylint: disable=too-many-positional-argu
     """
     from .upf import get_upf_content, is_soc_pseudo
 
-    reduced_pseudos = reduce_pseudos(pseudos, structure)
     if spin_orbit_coupling:
-        composition = structure.get_composition()
-        for kind in composition:
-            upf = reduced_pseudos[kind]
+        for site in structure.sites:
+            upf = pseudos[site.kind_name]
             upf_content = get_upf_content(upf)
             if not is_soc_pseudo(upf_content):
                 raise ValueError("Should use SOC pseudo for SOC calculation")
@@ -441,28 +437,31 @@ def get_number_of_projections_ext(
         raise ValueError(
             f"The type of structure is {type(structure)}, only aiida.orm.StructureData is accepted"
         )
-    # e.g. composition = {'Ga': 1, 'As': 1}
-    composition = structure.get_composition()
 
     # I use the first projector to detect SOCs
-    kind = list(composition.keys())[0]
+    kind = structure.get_kind_names()[0]
     spin_orbit_coupling_proj = "j" in external_projectors[kind][0]
     if spin_orbit_coupling and not spin_orbit_coupling_proj:
-        raise ValueError("SOC spin type is specified, however spin-unpolarized projectors are being used.")
+        raise ValueError(
+            "SOC spin type is specified, however spin-unpolarized projectors are being used."
+        )
     if spin_orbit_coupling_proj and not spin_orbit_coupling:
-        raise ValueError("spin-unpolarized spin type is specified, however SOC projectors are being used")
+        raise ValueError(
+            "spin-unpolarized spin type is specified, however SOC projectors are being used"
+        )
 
     tot_nprojs = 0
-    for kind in composition:
+    for site in structure.sites:
         nprojs = 0
-        for orb in external_projectors[kind]:
+        element = structure.get_kind(site.kind_name).symbol
+        for orb in external_projectors[element]:
             if spin_orbit_coupling:
                 nprojs += round(2 * orb["j"]) + 1
             else:
                 nprojs += 2 * orb["l"] + 1
         if spin_non_collinear and not spin_orbit_coupling:
             nprojs *= 2
-        tot_nprojs += nprojs * composition[kind]
+        tot_nprojs += nprojs
 
     return tot_nprojs
 
@@ -504,11 +503,15 @@ def get_projections(
             )
 
     projections = []
-    reduced_pseudos = reduce_pseudos(pseudos, structure)
-    # e.g. composition = {'Ga': 1, 'As': 1}
-    composition = structure.get_composition()
-    for kind in composition:
-        upf = reduced_pseudos[kind]
+    # I am not sure if it will cause some issues,
+    # since `get_projections_from_upf` will return the element name as key
+    # instead of `kind_name`. It may repreatly extend the `projections` list.
+    # e.g. get_projections_from_upf(Mn0) = ['Mn: s', 'Mn: p', 'Mn: d'],
+    # and get_projections_from_upf(Mn1) = ['Mn: s', 'Mn: p', 'Mn: d'] will repeat.
+    # However, get_projections do not used in AWW,
+    # I do not know where it may be used and can not do any test.
+    for kind_name in structure.get_kind_names():
+        upf = pseudos[kind_name]
         projs = get_projections_from_upf(upf)
         projections.extend(projs)
     return projections
@@ -560,40 +563,40 @@ def get_number_of_electrons(
     return tot_nelecs
 
 
-def reduce_pseudos(
-    pseudos: ty.Mapping[str, orm.UpfData],
-    structure: ty.Optional[orm.StructureData] = None,
-):
-    """Reduce pseudos from merging kinds with different kind names.
+# def reduce_pseudos(
+#     pseudos: ty.Mapping[str, orm.UpfData],
+#     structure: ty.Optional[orm.StructureData] = None,
+# ):
+#     """Reduce pseudos from merging kinds with different kind names.
 
-    Considering pseudofamily.get_pseudos may give
-    {'Fe0': <UpfData>,
-     'Fe1': <UpfData>}
-    However the structure.get_composion will give
-    {'Fe': 2}
-    This function will reduce pseudos to
-    {'Fe': <UpfData>}
-    """
+#     Considering pseudofamily.get_pseudos may give
+#     {'Fe0': <UpfData>,
+#      'Fe1': <UpfData>}
+#     However the structure.get_composion will give
+#     {'Fe': 2}
+#     This function will reduce pseudos to
+#     {'Fe': <UpfData>}
+#     """
 
-    reduced_pseudos = {}
-    if structure is not None:
-        for kind in structure.kinds:
-            if kind.name not in pseudos:
-                raise ValueError(f"{kind.name} do not match the pesudos.\n", pseudos)
-            if kind.symbol not in reduced_pseudos:
-                reduced_pseudos[kind.symbol] = pseudos[kind.name]
-            else:
-                if reduced_pseudos[kind.symbol].md5 != pseudos[kind.name].md5:
-                    raise NotImplementedError(
-                        "Same element different pseudos is not implement."
-                    )
-    else:
-        for _, val in pseudos.items():
-            if val.element not in reduced_pseudos:
-                reduced_pseudos[val.element] = val
-            else:
-                if val != reduced_pseudos[val.element]:
-                    raise NotImplementedError(
-                        "Same element different pseudos is not implement."
-                    )
-    return reduced_pseudos
+#     reduced_pseudos = {}
+#     if structure is not None:
+#         for kind in structure.kinds:
+#             if kind.name not in pseudos:
+#                 raise ValueError(f"{kind.name} do not match the pesudos.\n", pseudos)
+#             if kind.symbol not in reduced_pseudos:
+#                 reduced_pseudos[kind.symbol] = pseudos[kind.name]
+#             else:
+#                 if reduced_pseudos[kind.symbol].md5 != pseudos[kind.name].md5:
+#                     raise NotImplementedError(
+#                         "Same element different pseudos is not implement."
+#                     )
+#     else:
+#         for _, val in pseudos.items():
+#             if val.element not in reduced_pseudos:
+#                 reduced_pseudos[val.element] = val
+#             else:
+#                 if val != reduced_pseudos[val.element]:
+#                     raise NotImplementedError(
+#                         "Same element different pseudos is not implement."
+#                     )
+#     return reduced_pseudos
