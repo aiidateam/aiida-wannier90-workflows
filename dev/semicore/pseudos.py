64e6b5bb44bb169155320af65c45cc3cb2c66c8f
npaulish <nataliya.paulish@psi.ch>
2025-07-24 18:45:23 +0200

    Add a function for semicore list generation supporting UPF v1 and v2   * Add __init__.py file to the semicore dolder so the location can be deduced from the import   * Generate the semicore data for SSSP/1.3/PBEsol/efficiency
    When generating a semicore file, put an empty line in the end.

--- dev/semicore/pseudos.py
+++ dev/semicore/pseudos.py
@@ -2,7 +2,7 @@
 """Generate the semicore JSON for a given pseudo family."""
 from collections import OrderedDict
 import hashlib
-from importlib.resources import path
+from importlib.resources import files
 import json
 
 from aiida_pseudo.data.pseudo.upf import UpfData
@@ -51,7 +51,75 @@ def generate_semicore_dict(pseudo: UpfData, semicore_threshold=-1.8) -> dict:
     }
 
 
-def cli(pseudo_family: str, semicore_threshold: float = -1.8):
+def generate_semicore_dict_from_orbitals(pseudo: UpfData) -> dict:
+    """Generate the semicore dict based on the orbitals in the pseudo.
+
+    Take into account the element's position in the periodic table.
+    """
+    from ase.data import atomic_numbers
+
+    # P-block elements
+    pblock = (
+        list(range(31, 37))
+        + list(range(49, 55))
+        + list(range(81, 87))
+        + list(range(113, 119))
+    )
+
+    upf = UPFDict.from_str(pseudo.get_content())
+    filename = pseudo.filename
+    md5_hash = hashlib.md5(pseudo.get_content().encode()).hexdigest()
+    znum = atomic_numbers[upf["header"]["element"].strip()]
+    pswfcs_list = []
+    for wfc in upf["pswfc"]["chi"]:
+        # Calculate the rank based on the atomic number (Z) and orbital type
+        # for S and P orbitals of elements in P-block we add 1 to the rank,
+        # for D and F orbitals we add 1 and 2 respectively (l-1)
+        rank = int(wfc["label"][0])  # Extract the main quantum number
+        l = int(wfc["l"])
+        if znum in pblock and l in (0, 1):
+            rank += 1
+        elif l > 1:
+            rank += l - 1
+
+        pswfcs_list.append(
+            {
+                "label": wfc["label"],
+                "rank": rank,
+            }
+        )
+    return {
+        upf["header"]["element"].strip(): {
+            "filename": filename,
+            "md5": md5_hash,
+            "pswfcs": [wfc["label"] for wfc in pswfcs_list],
+            "semicores": [
+                wfc["label"]
+                for wfc in pswfcs_list
+                if wfc["rank"] < max(wfc["rank"] for wfc in pswfcs_list)
+            ],
+        }
+    }
+
+
+def generate_semicore_dict_auto(
+    pseudo: UpfData, method: str = "energy_threshold", semicore_threshold: float = -1.8
+) -> dict:
+    """Generate the semicore dict for a given pseudo potential using the specified method."""
+    if method == "energy_threshold":
+        return generate_semicore_dict(pseudo, semicore_threshold)
+    if method == "orbitals":
+        return generate_semicore_dict_from_orbitals(pseudo)
+    raise ValueError(
+        f"Unknown method: {method}\nAvailable methods: 'energy_threshold', 'orbitals'."
+    )
+
+
+def cli(
+    pseudo_family: str,
+    method: str = "energy_threshold",
+    semicore_threshold: float = -1.8,
+):
     """Generate the semicore JSON for a given pseudo family."""
     pseudo_group = orm.load_group(pseudo_family)
 
@@ -61,14 +129,17 @@ def cli(pseudo_family: str, semicore_threshold: float = -1.8):
         pseudo_group.pseudos.values(),
         description=f"Generating semicore dict for {pseudo_family}",
     ):
-        semicore_dict.update(generate_semicore_dict(pseudo, semicore_threshold))
+        semicore_dict.update(
+            generate_semicore_dict_auto(pseudo, method, semicore_threshold)
+        )
 
-    with path(semicore, f"{pseudo_family.replace('/', '_')}.json") as semicore_path:
-        with semicore_path.open("w", encoding="utf8") as handle:
-            json.dump(OrderedDict(sorted(semicore_dict.items())), handle, indent=4)
+    semicore_path = files(semicore).joinpath(f"{pseudo_family.replace('/', '_')}.json")
+    with open(semicore_path, "w", encoding="utf8") as handle:
+        json.dump(OrderedDict(sorted(semicore_dict.items())), handle, indent=4)
+        handle.write("\n")
 
-        print("[bold green]Success:[/] the semicore JSON has been generated at:\n")
-        print(f"  {semicore_path.absolute()}\n")
+    print("[bold green]Success:[/] the semicore JSON has been generated at:\n")
+    print(f"  {semicore_path.absolute()}\n")
 
 
 if __name__ == "__main__":
